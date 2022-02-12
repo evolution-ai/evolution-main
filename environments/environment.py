b83b5a3e84040082f1ccfb51c588b2e16d8dc035
Aaron Jeyaraj <aaron_jeyaraj@brown.edu>
2022-02-11 19:27:33 -0500

    init

---
+++ environments/environment.py
@@ -0,0 +1,24 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import pygame
+
+# Move
+# Environment
+# Position
+
+from basic_agent import *
+
+
+class Environment:
+    def __init__(self):
+        # initialize the world -> variables that determine layout, strcutrue, etc.
+        # store agent -> position in world
+        pass
+
+    def possible_moves(agent):
+        # List(Move)
+        pass
+    
+    
