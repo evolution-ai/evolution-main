b07771feb827a208b5775b99cd3dbc31ecfea68c
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2022-02-20 11:03:48 -0500

    actor critic trained (DO NOT TOUCH)

--- actor_critic_basic/ac1_main.py
+++ actor_critic_basic/ac1_main.py
@@ -11,19 +11,26 @@ from tensorflow.keras.optimizers import Adam
 import tensorflow_probability as tfp
 import time
 
-# from utils import plot_learning_curve
+# from actor_critic_basic.utils import plot_learning_curve
 
 from ac1_actors import Agent
 
 
+def plot_learning_curve(x, scores, figure_file):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 100 scores')
+    plt.savefig(figure_file)
+
+
 if __name__ == '__main__':
 
-    to_render = False
-
-    env = gym.make('CartPole-v0')
+    env = gym.make('CartPole-v1')
 
     agent = Agent(alpha = 1e-5, n_actions=env.action_space.n)
-    n_games = 3
+    n_games = 5
 
     filename = 'cartpole.png'
     figure_file = 'plots/' + filename
@@ -31,7 +38,8 @@ if __name__ == '__main__':
     best_score = env.reward_range[0]
     score_history = []
     avg_score_history = []
-    load_checkpoint = False
+    load_checkpoint = True
+    to_render = True
 
     if load_checkpoint:
         agent.load_models()
@@ -44,9 +52,9 @@ if __name__ == '__main__':
 
         while not done:
 
-            if to_render:
+            if to_render and (i % 1 == 0):
                 env.render()
-                time.sleep(0.002)
+                time.sleep(0.05)
 
             action = agent.choose_action(observation)
             observation_n, reward, done, info = env.step(action)
@@ -76,12 +84,7 @@ if __name__ == '__main__':
         env.close()
 
     # x = [i+1 for i in range(n_games)]
-    # plt.plot(x, avg_score_history)
-    # plt.xlabel('Episodes')
-    # plt.ylabel('Score')
-    # plt.show()
-    # plt.savefig('score_history.png')
     # plot_learning_curve(x, score_history, figure_file)
-    print(score_history)
+    # print(score_history)
 
 
