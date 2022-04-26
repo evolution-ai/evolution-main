9715906b484d9ec2a32a73b9c0c4015007f69e49
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2022-04-26 16:38:57 -0400

    tried import sys for render

--- ma-gym-v0/runner.py
+++ ma-gym-v0/runner.py
@@ -1,6 +1,11 @@
 import gym
 from stable_baselines3 import PPO
 
+import sys
+
+sys.path
+sys.executable
+
 env = gym.make("CartPole-v1")
 
 model = PPO("MlpPolicy", env, verbose=1)
@@ -8,7 +13,7 @@ model.learn(total_timesteps=10000)
 
 obs = env.reset()
 for i in range(1000):
-  
+
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
     env.render()
