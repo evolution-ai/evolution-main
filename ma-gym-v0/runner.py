29f41c71db1e9495c5ea3201f589a869222b636f
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2022-04-26 16:55:17 -0400

    Update runner.py

--- ma-gym-v0/runner.py
+++ ma-gym-v0/runner.py
@@ -8,8 +8,8 @@ sys.executable
 
 env = gym.make("CartPole-v1")
 
-model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+model = PPO("MlpPolicy", env, verbose=0)
+model.learn(total_timesteps=100)
 
 obs = env.reset()
 for i in range(1000):
