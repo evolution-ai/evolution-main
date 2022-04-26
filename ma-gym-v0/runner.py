400895840c8b63fabcf0f63bdd5fa5ec6cc18c30
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2022-04-26 16:36:58 -0400

    openAI gym

--- ma-gym-v0/runner.py
+++ ma-gym-v0/runner.py
@@ -8,9 +8,11 @@ model.learn(total_timesteps=10000)
 
 obs = env.reset()
 for i in range(1000):
+  
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
     env.render()
+
     if done:
       obs = env.reset()
 
