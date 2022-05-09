574bc5249ddfb4d9aec7e187764c373a0bc3c24f
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2022-05-09 00:04:45 -0400

    more reult csvs

--- reward_entropy/tester.py
+++ reward_entropy/tester.py
@@ -6,10 +6,10 @@ import time
 
 print("hi")
 
-time.sleep(100)
+telegram_send.send(messages=["Sound Test"])
 
+time.sleep(100)
 telegram_send.send(messages=["Error: Training Failed after 1023 Epochs", "Average Score = 32.43, Average Entropy = 0.52"])
 
 time.sleep(10)
-
 telegram_send.send(messages=["Rerunning Model"])
