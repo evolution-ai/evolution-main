2b9e3a4188ce01c46e2f3514524e7868159747f4
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2022-05-08 20:02:29 -0400

    fixed seg fault

--- reward_entropy/tester.py
+++ reward_entropy/tester.py
@@ -1,15 +1,15 @@
 import numpy as np
 import csv
 import telegram_send
-import keyboard
+# import keyboard
 import time
 
 print("hi")
 
-# time.sleep(500)
+time.sleep(100)
 
-# telegram_send.send(messages=["Error: Training Failed after 1023 Epochs", "Average Score = 32.43, Average Entropy = 0.52"])
+telegram_send.send(messages=["Error: Training Failed after 1023 Epochs", "Average Score = 32.43, Average Entropy = 0.52"])
 
-# time.sleep(10)
+time.sleep(10)
 
-# telegram_send.send(messages=["Rerunning Model"])
+telegram_send.send(messages=["Rerunning Model"])
